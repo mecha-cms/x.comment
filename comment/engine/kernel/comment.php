8aef0c7420988c7bde4431a828f467eb3a0af8fc
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-06-08 12:45:59 +0700

    Auto-Time

--- comment/engine/kernel/comment.php
+++ comment/engine/kernel/comment.php
@@ -3,13 +3,7 @@
 class Comment extends Page {
 
     public function __construct($input = null, $lot = [], $NS = 'comment') {
-        $time = is_array($input) ? (array_key_exists('time', $input) ? $input['time'] : date(DATE_WISE)) : (new Date(Path::N($input)))->format(DATE_WISE);
-        $date = new Date($time);
-        parent::__construct($input, array_merge([
-            'time' => $time,
-            'date' => $date,
-            'id' => (string) $date->unix
-        ], $lot), $NS);
+        parent::__construct($input, $lot, $NS);
     }
 
 }
