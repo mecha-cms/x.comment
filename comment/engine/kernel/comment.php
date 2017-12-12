0505768c4b2aa98b8e747ab0f2f85aba0c6f5a94
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-12-12 21:10:59 +0700

    @mecha-cms/genome/engine/kernel/page.php

--- comment/engine/kernel/comment.php
+++ comment/engine/kernel/comment.php
@@ -1,13 +1,3 @@
 <?php
 
-class Comment extends Page {
-
-    public function __construct($input = null, $lot = [], $NS = ['*', 'comment']) {
-        parent::__construct($input, $lot, $NS);
-    }
-
-    public static function open($path, $lot = [], $NS = ['*', 'comment']) {
-        return parent::open($path, $lot, $NS);
-    }
-
-}
+class Comment extends Page {}
