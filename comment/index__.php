a931885900356ef9afa883b4c72a52708484d4b7
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-05-21 16:58:26 +0700

    Remove Inline JavaScript

--- comment/index__.php
+++ comment/index__.php
@@ -7,21 +7,15 @@ if (!Folder::exist($f)) {
     Guardian::kick($url->current);
 }
 
-Hook::set('asset:head', function($content) use($site) {
-    if ($site->is('page')) {
-        $o = array_replace([
-            'id' => Extend::state('comment', 'anchor')[1]
-        ], (array) a(Config::get('page.o.js.COMMENT', [])));
-        return $content . '<script>window.COMMENT=' . json_encode($o) . ';</script>';
-    }
-    return $content;
-}, 9.9);
-
 Hook::set('shield.enter', function() use($site) {
     if ($site->is('page')) {
         $s = __DIR__ . DS . 'lot' . DS . 'asset' . DS;
         Asset::set($s . 'css' . DS . 'comment.min.css', 10);
-        Asset::set($s . 'js' . DS . 'comment.min.js', 10);
+        Asset::set($s . 'js' . DS . 'comment.min.js', 10, [
+            'src' => function($src) {
+                return $src . '#' . Extend::state('comment', 'anchor')[1];
+            }
+        ]);
     }
 }, 0);
 
--- comment/lot/asset/js/comment.js
+++ comment/lot/asset/js/comment.js
@@ -1,11 +1,13 @@
-(function($, win, doc) {
+(function(win, doc) {
 
-    var form = doc.getElementById($.id);
+    var script = doc.currentScript || doc.getElementsByTagName('script').pop(),
+        src = script.src,
+        id = src.split('#')[1],
+        form = id && doc.getElementById(id);
 
     if (!form) return;
 
-    var hooks = {}, param,
-        footer = form.parentNode,
+    var footer = form.parentNode,
         q = win.location.search,
         a = doc.getElementsByClassName('comment-reply:v'),
         x = form.getElementsByClassName('comment-reply:x')[0],
@@ -13,42 +15,8 @@
         content_placeholder = content.placeholder,
         test = /(\?|&(?:amp;)?)parent=\d+/g,
         parent = form.parent, i, j;
-        q = !q || !q.match(test);
 
-    function set(name, fn) {
-        name = name.split('.');
-        if (!hooks[name[0]]) hooks[name[0]] = {};
-        var i = name[1] || Object.keys(hooks[name[0]]).length;
-        hooks[name[0]][i] = fn;
-    }
-
-    function reset(name) {
-        if (!name) {
-            return hooks = {};
-        }
-        name = name.split('.');
-        if (name[1]) {
-            delete hooks[name[0]][name[1]];
-        } else {
-            delete hooks[name[0]];
-        }
-    }
-
-    function fire(name, param) {
-        name = name.split('.');
-        if (!hooks[name[0]]) hooks[name[0]] = {};
-        if (name[1]) {
-            if (hooks[name[0]][name[1]]) hooks[name[0]][name[1]].apply(form, param);
-        } else {
-            for (var i in hooks[name[0]]) {
-                hooks[name[0]][i].apply(form, param);
-            }
-        }
-    }
-
-    $.set = set;
-    $.reset = reset;
-    $.fire = fire;
+    q = !q || !q.match(test);
 
     function reply(a) {
         a.addEventListener('click', function(e) {
@@ -63,9 +31,6 @@
             content.placeholder = this.title;
             content.focus();
             parent.value = i;
-            param = [e, this];
-            fire('on.comment.reply', param);
-            fire('on.comment.reply:v', param);
             e.preventDefault();
         }, false);
     }
@@ -81,12 +46,13 @@
                 content.placeholder = content_placeholder;
                 content.focus();
                 parent.removeAttribute('value');
-                param = [e, this];
-                fire('on.comment.reply', param);
-                fire('on.comment.reply:x', param);
                 e.preventDefault();
             }, false);
         }
     }
 
-})(window.COMMENT || {}, window, document);
+    win.COMMENT = {
+        form: form
+    };
+
+})(window, document);
