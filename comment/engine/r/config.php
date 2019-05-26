19d056a6d8f29be2f301179a6396d34b73b8170f
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-05-26 08:03:06 +0700

    Update

--- comment/engine/r/config.php
+++ comment/engine/r/config.php
@@ -4,6 +4,6 @@
 $state = extend('comment');
 if (!empty($state['comment'])) {
     // Prioritize default state
-    Config::alt($state);
+    Config::over($state);
     Comment::$data = array_replace_recursive(Page::$data, (array) Config::get('comment', true));
 }
