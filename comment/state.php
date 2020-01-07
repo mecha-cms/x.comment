d23f86010c597b75d836b2676fbf775bae6f3eb4
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-01-07 23:47:13 +0700

    Update

--- comment/state.php
+++ comment/state.php
@@ -5,7 +5,7 @@ return [
         // Default file extension for new comment (`draft` to save/moderate the comment and `page` to publish the comment immediately)
         'x' => 'page'
     ],
-    'anchor' => ['comment-%s', 'form-comment', 'comments'],
+    'anchor' => ['comment:%s', 'form-comment', 'comments'],
     'chunk' => 9999, // TODO: Comment pagination
     'deep' => 2, // Set to `0` to disable comment thread, `1` or more to enable comment thread
     'user' => true, // Show log-in button if user extension is available
