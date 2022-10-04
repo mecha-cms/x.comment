f62a0b3b3d1afefca1a33850e1c3f25e96333a82
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-10-04 12:58:47 +0700

    Update

--- engine/plug/comment.php
+++ engine/plug/comment.php
@@ -1,9 +1,5 @@
 <?php
 
-function comment(...$v) {
-    return new Comment(...$v);
-}
-
-function comments(...$v) {
-    return Comments::from(...$v);
+function comment(...$lot) {
+    return Comment::from(...$lot);
 }
---
+++ engine/plug/comments.php
@@ -0,0 +1,5 @@
+<?php
+
+function comments(...$lot) {
+    return Comments::from(...$lot);
+}
