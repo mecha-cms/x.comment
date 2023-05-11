fd6100598c41b09a68ee62b80d2da282612cea65
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-05-11 22:05:18 +0700

    Update

--- engine/kernel/comments.php
+++ engine/kernel/comments.php
@@ -8,9 +8,6 @@ class Comments extends Pages {
     }
 
     public static function from(...$lot) {
-        if (is_array($v = reset($lot))) {
-            return parent::from($v);
-        }
         $lot[0] = $lot[0] ?? LOT . D . 'comment';
         return parent::from(...$lot);
     }
